644d76f2aae3a73702953a4e7e5b53b9324b233e
Kubonka <srbetito85@gmail.com>
2025-07-27 21:09:11 -0300

    v0.3

--- src/app/scrable/page.tsx
+++ src/app/scrable/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Button } from "@/components/ui/button";
