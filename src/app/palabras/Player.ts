214d9799dfa7327fd935ead40be771e25c8c2cae
Kubonka <srbetito85@gmail.com>
2025-08-02 18:31:29 -0300

    rpg_on

--- src/app/palabras/Player.ts
+++ src/app/palabras/Player.ts
@@ -11,7 +11,7 @@ export default class Player {
   //todo implementar una memoria de lo ultimo
   //todo implementar un solo uso de habilidad
   //todo implementar la habilidad en modo gris
-  constructor(initialSkillPoints: number = 7) {
+  constructor(initialSkillPoints: number = 0) {
     this.skillPoints = initialSkillPoints;
     this.skills = [];
     this.addSkill(new CharRevealGreen());
